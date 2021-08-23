95cece37bf0dd4b833958fd28c0037e981f4b4cb
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-08-23 22:16:29 +0530

    Prob2: triangle quiz

---
+++ triangleQuiz/quiz.js
@@ -0,0 +1,17 @@
+console.log("hello from JavaScript");
+
+// get the quiz form 
+const quizForm = document.querySelector(".quiz-form");
+console.log(quizForm);
+
+// get the submit answer button
+const submitAnswerButton = document.querySelector("#submit-answers-btn");
+console.log(submitAnswerButton);
+
+// get the output div
+const outputMessage = document.querySelector("#output");
+console.log(outputMessage);
+
+
+//Now, create an array of answers for each question.
+// This array is used to compare the correct answer with the user checked answer.
